94cceda2219d85cc088ce29a06d702d44de1f5d3
Sofiia <46069177+SmahliukSofiia@users.noreply.github.com>
2019-01-18 14:04:11 +0200

    Update script.js
    group

--- 6/script.js
+++ 6/script.js
@@ -2,15 +2,16 @@ var letters = 'abcdefghijklmnopqrstuvwxyz',
     userNumber = +prompt('How many values ​​need to create?'),
     minNumber = +prompt('Minimum number of characters'),
     maxNumber = +prompt('Maximum number of characters'),
-    arr = [];
+    arr = [],
+    obj = {};
 
 for (var number = 0; number < userNumber; number++) {
     var text = '';
     for (var i = 0; i < Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber; i++) {
         text += letters.charAt(Math.floor(Math.random() * letters.length));
     }  
-    arr.push(text);
+    obj[text.length] = obj[text.length] ? obj[text.length].concat([text]) : [text];
 }
-console.log(arr);
+console.log(obj);
 
 
