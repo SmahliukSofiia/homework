42d5d30b38ae45de4b40b36eddf9e2df5ae7b84f
Sofiia <46069177+SmahliukSofiia@users.noreply.github.com>
2019-01-29 09:20:53 +0200

    Update script.js
    Fixed timer

--- 9/script.js
+++ 9/script.js
@@ -20,8 +20,28 @@ function checkTime(i) {
     return (i < 10) ? ('0' + i) : i;
 }
 
+function compare() {
+    var currentDate = new Date(),
+        dateForTask1 = new Date(),
+        dateForTask2 = new Date(),
+        dateForTask3 = new Date();
+        dateForTask1.setHours(19, 4, 0);
+        dateForTask2.setHours(19, 4, 30);
+        dateForTask3.setHours(18, 52, 0);
 
-function timer1() {
+    if (Math.floor(currentDate.getTime() / 1000) === Math.floor(dateForTask1.getTime() / 1000)) {
+        document.getElementById('task').innerText = 'Hello ☺️';
+    } if (Math.floor(currentDate.getTime() / 1000) === Math.floor(dateForTask2.getTime() / 1000)){
+        document.getElementById('task').innerText = 'Have a nice day ✨';
+    } if (Math.floor(currentDate.getTime() / 1000) === Math.floor(dateForTask3.getTime() / 1000)) {
+        document.getElementById('task').innerText = '❄️ ❄️ ❄️ ❄️ ❄️ ❄️ ❄️ ❄️ ';
+    }
+    setTimeout(compare, 1000);
+}
+
+compare()
+
+/*function timer1() {
     var timeForTask = new Date(),
         hoursForTask = timeForTask.setHours(16),
         minutesForTask = timeForTask.setMinutes(7),
@@ -61,4 +81,4 @@ function compare() {
     setTimeout(compare, 1000);
 }
 
-compare()
+compare() */
