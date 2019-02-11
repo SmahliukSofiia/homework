4380ba4d20d8f543ff95e2f1221c6c41e196ddde
Sofiia <46069177+SmahliukSofiia@users.noreply.github.com>
2019-02-11 15:35:41 +0200

    Create constructor.js
    Add 11 homework

---
+++ 11/constructor.js
@@ -0,0 +1,83 @@
+function random(min, max) {
+    return Math.round(Math.random() * (max - min) + min)
+}
+
+function Company() {
+    this.stuff = [];
+}
+
+Company.levels = [
+    {
+        name: 'Junior',
+        salaryFrom: 300,
+        salaryTo: 550,
+    }, {
+        name: 'Middle',
+        salaryFrom: 80000,
+        salaryTo: 40000000,
+    }, {
+        name: 'Senior',
+        salaryFrom: 150000,
+        salaryTo: 300000000,
+    }
+]
+
+Company.prototype.add = function (emp) {
+    this.stuff.push({
+        employee: emp,
+        level: Company.levels[random(0, 2)]
+    });
+}
+
+Company.prototype.remove = function (emp) {
+    this.stuff.splice(this.stuff.findIndex(function (el) {
+        return el.employee === emp;
+}), 1)}
+
+Company.prototype.getSalarySum = function() {
+    return this.stuff.reduce(function(acc, next) {
+        return acc + next.employee.salary;
+    }, 0);
+}
+
+Company.prototype.getAccountInfo = function() {
+    return this.stuff.map(function (developer) {
+        return new AccountInfo(developer.employee);
+    })
+}
+
+Company.prototype.getResumeInfo = function (resumeEmp) {
+    return new ResumeInfo(resumeEmp);
+}
+
+Company.prototype.filterTechnology = function(tech) {
+    return this.stuff.filter(function (cmp) {
+        return cmp.employee.technology === tech;
+    })
+}
+
+function Employee(id, name, surname, salary, technology, startDate) {
+    this.id = id;
+    this.name = name;
+    this.surname = surname;
+    this.salary = salary;
+    this.technology = technology;
+    this.startDate = startDate;
+    this.isOccupied = false;
+}
+
+Employee.tech = ['JS', 'PHP', '.Net', 'Java', 'Scala', 'C++', 'C', 'Swift', 'Delphi', 'HTML', 'DevOps', 'Administrator']
+month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
+    'September', 'October', 'November', 'December']
+
+function AccountInfo(user) {
+    this.name = user.name + ' ' + user.surname;
+    this.tax = Math.round(user.salary * .05);
+    this.salary = user.salary;
+}
+
+function ResumeInfo(emp) {
+    this.name = emp.name + ' ' + emp.surname;
+    this.technology = emp.technology;
+    this.salary = emp.salary;
+} 
