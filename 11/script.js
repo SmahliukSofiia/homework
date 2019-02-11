67e887bdc1d0cb51356cb92207a31f81670534e2
Sofiia <46069177+SmahliukSofiia@users.noreply.github.com>
2019-02-11 15:33:54 +0200

    Create script.js
    Add 11 homework

---
+++ 11/script.js
@@ -0,0 +1,34 @@
+var company = new Company();
+
+var emp = new Employee (
+    1,
+    'Random 1',
+    'Random Sur 1',
+    random(1000, 5000),
+    Employee.tech[random(0, Employee.tech.length - 1)],
+    random(1, 30) + ' ' + month[random(0, month.length - 1)] + ' ' + random(1994, 2019)
+)
+
+var emp2 = new Employee (
+    2,
+    'Random 2',
+    'Random Sur 2',
+    random(1000, 5000),
+    Employee.tech[random(0, Employee.tech.length - 1)],
+    random(1, 30) + ' ' + month[random(0, month.length - 1)] + ' ' + random(1994, 2019)
+)
+
+company.add(emp)
+company.add(emp2)
+
+var accInfo = company.getAccountInfo();
+var salarySum = company.getSalarySum();
+
+var resume = company.getResumeInfo(company.stuff[0].employee);
+var empTech = company.filterTechnology('C');
+
+console.log(company)
+console.log(accInfo)
+console.log(salarySum)
+console.log(resume)
+console.log(empTech)
