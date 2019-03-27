7cce969a7d2bcf9f9a7a1a809f030b4a2b190e02
Sofiia <46069177+SmahliukSofiia@users.noreply.github.com>
2019-03-27 12:37:24 +0200

    Update script.js
    add prototypes

--- 16/script.js
+++ 16/script.js
@@ -1,57 +1,106 @@
-function httpGet(url, cb) {
-    var xhr = new XMLHttpRequest();
+function Repository() {
 
-    xhr.open('get', url, true);
-    xhr.addEventListener('load', function(e) {
-        var result = JSON.parse(e.target.response);
-        console.log(result)
-        cb(result);
-    });
-    xhr.send();
+};
+
+Repository.prototype = {
+    get: function(url, cb) {
+        var xhr = new XMLHttpRequest();
+
+        xhr.open('get', url, true);
+        xhr.addEventListener('load', function(e) {
+            var result = JSON.parse(e.target.response);
+            console.log(result)
+            cb(result);
+        });
+        xhr.send();
+    }
+};
+
+var rep = new Repository();
+
+
+function Cards() {
+
 }
 
-function collectData(url, page) {
-        httpGet(url + '?page=' + page, function showHeroes(result) {
-            var section = document.querySelector('section');
 
+var page = 1;
+
+var arr = [];
+
+Cards.prototype = {
+    collectData: function(url, page) {
+        rep.get(url + '?page=' + page, function getHeroes(result) {
             var heroes = result.results;
-            
-            for (var i = 0; i < heroes.length; i++) {
-                var myArticle = document.createElement('article');
-                var myH2 = document.createElement('h2');
-                var myP1 = document.createElement('p');
-                var myP2 = document.createElement('p');
-
-                myH2.textContent = heroes[i].name;
-                myP1.textContent = 'Birth year: ' + heroes[i].birth_year + '\n\r' + 'Gender: ' + heroes[i].gender + '\n\r'
-                    + 'Height: ' + heroes[i].height + '\n\r' + 'Mass: ' + heroes[i].mass;
-                myP2.textContent = 'Eye color: ' + heroes[i].eye_color + '\n\r' + 'Hair color: ' + heroes[i].hair_color 
-                    + '\n\r' + 'Skin color: ' + heroes[i].skin_color;
-
-                myArticle.appendChild(myH2);
-                myArticle.appendChild(myP1);
-                myArticle.appendChild(myP2);
-
-                section.appendChild(myArticle);
-            }
+            arr = arr.concat(result);
+            card.showHeroes(heroes);
         })
-        
-        // var myPage = page;
-        // myBtn.addEventListener('click', function() {
-        //     var page = myPage + 1;
-        //     r(page);
-        // })
-    
+    },
+
+    showHeroes: function(heroes) {
+        var section = document.createElement('section');
+            
+        for (var i = 0; i < heroes.length; i++) {
+            var myArticle = document.createElement('article');
+            var myH2 = document.createElement('h2');
+            var myP1 = document.createElement('p');
+            var myP2 = document.createElement('p');
+
+            myH2.textContent = heroes[i].name;
+            myP1.textContent = 'Birth year: ' + heroes[i].birth_year + '\n\r' + 'Gender: ' + heroes[i].gender + '\n\r'
+                + 'Height: ' + heroes[i].height + '\n\r' + 'Mass: ' + heroes[i].mass;
+            myP2.textContent = 'Eye color: ' + heroes[i].eye_color + '\n\r' + 'Hair color: ' + heroes[i].hair_color 
+                + '\n\r' + 'Skin color: ' + heroes[i].skin_color;
+
+            myArticle.appendChild(myH2);
+            myArticle.appendChild(myP1);
+            myArticle.appendChild(myP2);
+
+            section.appendChild(myArticle);
+        }
+        document.body.appendChild(section);
+    },
+
+    removeContent: function() {
+        var section = document.querySelector('section');
+        if (section) {
+            section.remove();
+        }
+    }
 }
 
-// collectData('https://swapi.co/api/people/', 1)
+var card = new Cards();
 
-var footer = document.querySelector('footer');
-var myBtn = document.createElement('button');
-myBtn.textContent = 'Next';
-footer.appendChild(myBtn);
 
-myBtn.addEventListener('click', function() {
-    var page = +page;
-    collectData('https://swapi.co/api/people/', 1)
+var header = document.querySelector('header');
+var myBtnP = document.createElement('button');
+myBtnP.textContent = 'Previous';
+header.appendChild(myBtnP);
+
+var header = document.querySelector('header');
+var myBtnN = document.createElement('button');
+myBtnN.textContent = 'Next';
+header.appendChild(myBtnN);
+
+
+myBtnN.addEventListener('click', function() {
+    if(page <= 9) {
+        if (arr.length < page) {
+            card.collectData('https://swapi.co/api/people/', page);
+        } else {
+            heroes = arr[page - 1].results;
+            card.showHeroes(heroes)
+        }
+        ++page;
+        card.removeContent();
+    }
 })
+
+myBtnP.addEventListener('click', function() {
+    if (page > 2) {
+        heroes = arr[page - 3].results;
+        card.showHeroes(heroes)
+        --page;
+        card.removeContent();
+    }
+}) 
